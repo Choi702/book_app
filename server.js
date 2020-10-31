241ae2c26a2ba7e8b54c925b8c1b07eb828f62b1
Choi702 <jaeychoi1@gmail.com>
2020-10-31 14:18:27 -0700

    another scaffold files

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
 'use strict'
+
+//bring in our dependencies
+
+const express = require('express');
+const superagent = require('superagent');
+const cors = require('cors');
+const { response } = require('express');
+
+require('dotenv').config();
+
+
+//create our Port
+
+const PORT = process.env.PORT || 3000;
+
+//start express application
+const app = express();
+app.use(cors());
+
+// where server look for pages for browser
+// app.use(express.static('./public'));
+
+//view engine
+// app.set('view engine', 'ejs');
+
+app.get('/', (request, response)=>{
+  response.status(200).send('Hello World');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is now listening on port ${PORT}`);
+});
