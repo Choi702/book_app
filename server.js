60fff6657a5022610890d51f901563890dfe28ac
Choi702 <jaeychoi1@gmail.com>
2020-10-31 18:56:01 -0700

    still working on feature 3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
-const { response } = require('express');
+const { response, urlencoded } = require('express');
 
 require('dotenv').config();
 
@@ -25,12 +25,55 @@ app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 app.get('/hello', (request, response)=>{
-  response.status(200).render('pages/index');
-})
+  response.status(200).render('pages/index');})
 // search new.ejs route
-app.get('/searches', (request, response)=>{
+app.get('/searches/new', (request, response)=>{
   response.render('pages/searches/new');
 })
+
+
+app.post('/searches/new', (request, response)=>{
+  const search = request.body.title1;
+  const authorTitle = request.body.authorOrTitle;
+  let urlAuthorTitle = '';
+  if(authorTitle === 'title'){
+    urlAuthorTitle = `intitle:${search}`;
+
+  }else if (authorOrTitle === 'author') {
+    urlAuthorTitle = `inauthor:${search}`;
+  }
+
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=${urlAuthorTitle}`;
+  superagent.get(URL)
+  .then(data => {
+    data.body.items.map(book =>{
+    new Book(book.volumeInfo);
+    })
+    .then(book => response.render('pages/searches/show', { searchResults: book })); 
+  }) 
+})
+
+
+//constructor function for Book
+
+function Book(obj){
+  this.author = obj.authors;
+  this.title = obj.title;
+  this.description = obj.description;
+  this.image = obj.imageLinks;
+  this.isbn = obj.industryIdentifiers.type;
+  this.id = obj.id;
+  const holderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+
+  // if(){
+  //   imageLink = newBook.volumeInfo.bookApi.thumbnail;
+  
+  // }else {
+  //   bookApi = 
+  // }
+}
+
+//route handler
 
 app.listen(PORT, () => {
   console.log(`Server is now listening on port ${PORT}`);
